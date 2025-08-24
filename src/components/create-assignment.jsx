313253fe6a3536dab511441d0228d5892f88d19d
CCVela1017 <carlosvelaeduardo@gmail.com>
2025-08-24 13:08:21 -0600

    feat(generator): created functionality to add new assignments

---
+++ src/components/create-assignment.jsx
@@ -0,0 +1,39 @@
+import { useState } from 'react'
+
+function AssignmentGenerator({ onDataSend }) {
+  const [count, setCount] = useState(1);
+  const [title, setTitle] = useState("");
+
+  const sendNewAssignment = () => {
+    if (title.trim() !== "") {
+      const newAssignment = { id: count, title: title };
+      onDataSend(newAssignment);
+    }
+  }
+
+  return (
+    <>
+      <div className="create-assignment">
+        <h2>Crear nueva tarea</h2>
+        <input 
+          type="text" 
+          placeholder="Título de la tarea" 
+          value={title} 
+          onChange={(e) => setTitle(e.target.value)} 
+        />
+        <button onClick={() => {
+          if (title.trim() !== "") {
+            setCount(count + 1);
+            sendNewAssignment();
+            setTitle("");
+          }
+        }}>
+          Añadir Tarea
+        </button>
+      </div>
+    </>
+  )
+  
+}
+
+export default AssignmentGenerator
--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import Assignment from './assignment.jsx'
+import AssignmentGenerator from './create-assignment.jsx';
 
 function Home() {
   const [assignments, setAssignments] = useState([
@@ -15,6 +16,7 @@ function Home() {
   return (
     <>
       <div className="home">
+        <AssignmentGenerator onDataSend={(newAssignment) => setAssignments([...assignments, newAssignment])} />
         <h1>Mis tareas</h1>
         {assignments.map(assignment => (
           <Assignment key={assignment.id} id={assignment.id} title={assignment.title} onDelete={deleteAssignment} />
