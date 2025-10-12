1bf4c8b42cb01bfba74c7ce4b410d433b0f341ec
CCVela1017 <carlosvelaeduardo@gmail.com>
2025-10-12 08:56:26 -0600

    feat: design for tabview for show-create components

--- frontend/src/components/home.jsx
+++ frontend/src/components/home.jsx
@@ -1,7 +1,34 @@
+import { useState } from 'react'
+
+
 function Home() {
+  const [activeTab, setActiveTab] = useState('create');
+
+  const renderTabs = () => {
+    switch (activeTab) {
+      case 'create':
+        return <div>Create Message Component</div>;
+      case 'show':
+        return <div>View Messages Component</div>;
+      default:
+        return null;
+    }
+  }
+
   return (
     <>
       <h1>Mensajes secretos</h1>
+      <p>Bienvenido a la aplicación de mensajes secretos.</p>
+
+      <div>
+        <button onClick={() => setActiveTab('create')}>
+          Crear mensaje
+        </button>
+        <button onClick={() => setActiveTab('show')}>
+          Mostrar mensaje
+        </button>
+      </div>
+      <div>{renderTabs()}</div>
     </>
   )
 }
